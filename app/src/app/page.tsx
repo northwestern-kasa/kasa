f8b1cda02d247a83d332c393f9e94dc1df9e8494
heochristopher <71462915+heochristopher@users.noreply.github.com>
2024-07-07 14:57:20 -0400

    Setup

---
+++ app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from './index.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+      <h1>Northwestern KASA</h1>
+    </main>
+  );
+}
