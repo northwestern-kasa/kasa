02ee25bcc7b3a272695f3f620fca255d515927d7
Toby Zheng <tobyjzheng@gmail.com>
2024-07-26 23:21:09 -0400

    Add existing file

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import styles from './index.module.css'
 import RedButton from "./components/RedButton";
 import ExecCard from "./components/ExecCard";
-import execImages from "./components/ExecCard/execData.json"
+import execImages from "./components/ExecCard/execData.json";
+import MemberDirCard_small from "./components/MemberDirCard_small";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <h1>Northwestern KASA</h1>
       <RedButton Text="Submit"/>
       <ExecCard images={execImages.execImages}/>
+      <MemberDirCard_small Name="Toby Zheng" Interest="KASA"/>
     </main>
   );
 }
