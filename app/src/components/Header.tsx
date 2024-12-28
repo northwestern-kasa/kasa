048f89299ab956df4ab2917de08e00699333bf66
heochristopher <71462915+heochristopher@users.noreply.github.com>
2024-12-28 18:52:43 -0500

    login ui

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -5,11 +5,11 @@ import profile from "../../assets/profile.png"
 
 export default function Header() {
     return (
-        <div id="container" className="fixed top-0 left-0 right-0 flex justify-between p-4">
+        <div id="container" className="fixed top-0 left-0 right-0 flex justify-between p-4 z-10">
             <Link href="/" className="w-10">
                 <Image src={logo} alt="Kasa's logo"/>
             </Link>
-            <Link href="/" className="w-10">
+            <Link href="/login" className="w-10">
                 <Image src={profile} alt="Default profile picture"/>
             </Link>
         </div>
