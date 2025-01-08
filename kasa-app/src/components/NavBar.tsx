53bb3413a86c2eb1d8c3d3a6c6289cd0e9e3b838
Anthony Chung <75187566+anthonytchung@users.noreply.github.com>
2025-01-07 23:10:23 -0600

    navbar larger

--- kasa-app/src/components/NavBar.tsx
+++ kasa-app/src/components/NavBar.tsx
@@ -3,9 +3,9 @@ import { Link, useLocation } from 'react-router-dom'
 export default function NavBar() {
     const currentPath = useLocation().pathname
     return (
-        <div id="container" className="flex space-x-12 rounded-full navBarShadow py-3 px-16 ">
+        <div id="container" className="flex navBarShadow space-x-20 rounded-full py-4 px-24 text-2xl font-sans">
             <Link to="/"><h3 className={currentPath == "/" ? "font-bold" : ""}>Home</h3></Link>
-            <Link to="/family"><h3 className={currentPath == "/family" ? "font-bold" : ""}>Family</h3></Link>
+            <Link to="/family"><h3 className={currentPath == "/family" ? "font-bold" : ""}>Families</h3></Link>
             <Link to="/events"><h3 className={currentPath == "/events" ? "font-bold" : ""}>Events</h3></Link>
             <Link to="/apply"><h3 className={currentPath == "/apply" ? "font-bold" : ""}>Apply</h3></Link>
             <Link to="/directory"><h3 className={currentPath == "/directory" ? "font-bold" : ""}>Directory</h3></Link>
