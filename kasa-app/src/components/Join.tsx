d1c5a1d22be588725dbd3ee62be05a4448296d3b
Anthony Chung <75187566+anthonytchung@users.noreply.github.com>
2025-07-24 16:28:27 -0400

    Changed Join to use hashrouter

--- kasa-app/src/components/Join.tsx
+++ kasa-app/src/components/Join.tsx
@@ -1,10 +1,11 @@
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 export default function Join() {
   return (
     <div className="flex items-center justify-center w-2/3 ">
       <Button asChild className="bg-[#ff3939cd] font-black text-black hover:bg-red/90 w-full border-blue border-2 h-full">
-        <a href="/apply">Apply</a>
+        <Link to="/apply">Apply</Link>
       </Button>
     </div>
   );
